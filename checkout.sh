c2001ea4d1d15eb39d09feb80d08e6b5cca857bb
Mark J. Becker <herzog31@users.noreply.github.com>
2024-02-22 11:11:45 +0100

    Add git credentials

--- checkout.sh
+++ checkout.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Hello World"
+echo "Hello World on seat ${SEAT}"
+
+# Create GitHub configs and credentials
+touch ~/.gitconfig
+
+git config --global credential.helper store
+git config --global user.name "Adobe Summit"
+git config --global user.email "no-reply@github.com"
+git config --global credential.https://github.com/adobe-summit-L445.username adobe-summit-l445-user
+cat ~/.gitconfig
+
+touch ~/.git-credentials
+echo "https://adobe-summit-l445-user:${GITHUB_TOKEN}@github.com" > ~/.git-credentials
+
+cat ~/.git-credentials
+
+# Checkout repository
+
+# Run npm install
